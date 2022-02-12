1d69507c6f36354b1a067f8e47ebbb7fb570329c
Matthew Tao <matthew.yingtao@gmail.com>
2022-02-12 18:59:10 +1300

    styled top level blog headings

--- src/pages/blog/{MarkdownRemark.frontmatter__slug}.jsx
+++ src/pages/blog/{MarkdownRemark.frontmatter__slug}.jsx
@@ -4,14 +4,17 @@ import Seo from "../../components/layout/seo"
 import "./blogPage.css"
 
 export default function Home({ data }) {
-  const { markdownRemark } = data
-  const { frontmatter, html } = markdownRemark
+  const {
+    markdownRemark: { frontmatter, html },
+  } = data
 
   return (
     <>
       <Seo />
-      <h1>{frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: html }} />
+      <h1 className="blogTitle">{frontmatter.title}</h1>
+      <article className="blogContent">
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      </article>
     </>
   )
 }
