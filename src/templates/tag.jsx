4945583e1814d44029bb3d4026c780aa0e22d25c
Matthew Tao <matthew.yingtao@gmail.com>
2022-02-19 21:29:14 +1300

    added back link to tag pages

--- src/templates/tag.jsx
+++ src/templates/tag.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import { graphql } from "gatsby";
 import { BlogPostList } from "../components/blogPostList";
+import { ArrowLink } from "../components/arrowLink";
 
 export default function Tags({ pageContext, data: { posts } }) {
   const { tag } = pageContext;
   const { totalCount } = posts;
   const tagHeader = `${totalCount} post${
     totalCount === 1 ? "" : "s"
-  } tagged with "${tag}"`;
+  } tagged with`;
 
   return (
     <>
-      <h2 style={{ marginBottom: "var(--space-xl)" }}>{tagHeader}</h2>
+      <ArrowLink
+        to="/blog"
+        directionRight={false}
+        style={{ marginBottom: "var(--space-lg)" }}
+      >
+        Back to all posts
+      </ArrowLink>
+      <h3 style={{ marginBottom: "var(--space-xl)" }}>
+        {tagHeader} "{tag}"
+      </h3>
       <BlogPostList posts={posts.edges.map(({ node }) => node)} />
     </>
   );
@@ -20,7 +30,6 @@ export default function Tags({ pageContext, data: { posts } }) {
 export const pageQuery = graphql`
   query ($tag: String) {
     posts: allMarkdownRemark(
-      limit: 2000
       sort: { fields: [frontmatter___date], order: DESC }
       filter: { frontmatter: { tags: { in: [$tag] } } }
     ) {
