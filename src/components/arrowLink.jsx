35eb0dccfd64ddb2ad644f88468dcc87735b2a15
Matthew Tao <matthew.yingtao@gmail.com>
2022-02-19 21:26:19 +1300

    added option for arrow link to be reversed

--- src/components/arrowLink.jsx
+++ src/components/arrowLink.jsx
@@ -1,12 +1,16 @@
 import * as React from "react";
 import { Link } from "gatsby";
-import { IoIosArrowRoundForward } from "react-icons/io";
-import { arrowLink as arrowLinkStyle } from "./arrowLink.module.css";
+import { IoIosArrowRoundForward, IoIosArrowRoundBack } from "react-icons/io";
+import { arrowLink as arrowLinkStyle, left } from "./arrowLink.module.css";
 
-export function ArrowLink({ to, children, ...props }) {
-  return (
+export function ArrowLink({ to, children, directionRight = true, ...props }) {
+  return directionRight ? (
     <Link to={to} className={arrowLinkStyle} {...props}>
       {children} <IoIosArrowRoundForward size={32} />
+    </Link>
+  ) : (
+    <Link to={to} className={[arrowLinkStyle, left].join(" ")} {...props}>
+      <IoIosArrowRoundBack size={32} /> {children}
     </Link>
   );
 }
